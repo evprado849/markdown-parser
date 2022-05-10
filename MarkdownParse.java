060ad3688221f275c54318f2aada0a9b34db65f2
Elijah V Prado <cs15lsp22asn@ieng6-201.ucsd.edu>
2022-05-09 17:37:28 -0700

    added method

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -7,6 +7,10 @@ import java.util.ArrayList;
 
 public class MarkdownParse {
 
+    public static String markdownTest() {
+        return "Testing a change in code MarkdownParse.";
+    } 
+    
     public static ArrayList<String> getLinks(String markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
         // find the next [, then find the ], then find the (, then read link upto next )
